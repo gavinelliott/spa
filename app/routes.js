6730df4261408f48fea4306d5e8a0ec8c7abddfb
Colin Oakley <colin@htmlandbacon.com>
2015-09-25 10:55:36 +0100

    added routes for redirect and place holders for pages

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,26 @@ module.exports = {
       res.render('examples/template-data', { 'name' : 'Foo' });
     });
 
-    // add your routes here
+    /* redirect routes for v4 */
+    app.get('/iteration1-v4/question2', function (req, res) {
+      if(req.query["radio-group"] != "Never been married"){
+          res.redirect("iteration1-v4/error")
+      }
+      res.render('iteration1-v4/question2.html');
+    });
 
+    app.get('/iteration1-v4/calculated', function (req, res) {
+      if(req.query["radio-inline-group"] == "Yes"){
+          res.redirect("iteration1-v4/error")
+      }
+      res.render('iteration1-v4/calculated.html');
+    });
+
+    app.get('/iteration1-v4/bank-details', function (req, res) {
+      if(req.query["radio-indent-group"] == "No"){
+          res.redirect("iteration1-v4/deffer")
+      }
+      res.render('iteration1-v4/bank-details.html');
+    });
   }
 };
